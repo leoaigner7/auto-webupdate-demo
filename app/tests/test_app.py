0f3b63ceab7258ed851276ebbd313534b67e5b37
Leo Aigner <leoaigner7@gmail.com>
2025-10-30 08:49:27 +0100

    Fix

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,10 +1,6 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from app.main import app
 
 def test_home_route():
     test_client = app.test_client()
     response = test_client.get("/")
     assert response.status_code == 200
-
