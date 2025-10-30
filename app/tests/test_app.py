638ae357d926ef58cf1b2bcb21a85cbd038e02d4
Leo Aigner <leoaigner7@gmail.com>
2025-10-30 08:29:56 +0100

    jz bassds

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,6 +1,10 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app.main import app
 
 def test_home_route():
-    client = app.test_client()
-    response = client.get("/")
+    test_client = app.test_client()
+    response = test_client.get("/")
     assert response.status_code == 200
+
